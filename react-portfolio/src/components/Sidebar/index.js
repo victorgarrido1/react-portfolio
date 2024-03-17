9061051748b0993fc81fcc5875faae39652698ae
victorgarrido1 <vcgarr94@gmail.com>
2024-03-16 19:35:29 -0700

    adding nav styingling phase one

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import './index.scss'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -10,11 +12,20 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt="logo"/>
-            <img className='sub-logo' src={LogoSubtitle} alt="logo-subtitle" >
-
-            </img>
-
+            <img className='sub-logo' src={LogoSubtitle} alt="logo-subtitle" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4de'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4de'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4de'/>
+            </NavLink>
+        </nav>
+
     </div>
 )
 
