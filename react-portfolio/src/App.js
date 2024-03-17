38eab0d466e30e064568f9df5580ed9e39e1cbde
victorgarrido1 <vcgarr94@gmail.com>
2024-03-17 12:47:22 -0700

    home page linking issue

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,13 +1,14 @@
+import LogoTitle from '../../assets/images/logo-s.png'; // Add missing import statement for React
 
-
-
-const Home = () =>  {
-
-
- return (
-
-    
- ) 
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm</h1> {/* Fix syntax error */}
+                <img src={LogoTitle} alt="developer" /> {/* Use the imported LogoTitle */}
+            </div>
+        </div> 
+    );
 }
 
 export default Home
