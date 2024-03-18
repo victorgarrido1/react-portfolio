afd4992081bbf83fd8caf11379094c7a02fe9f02
victorgarrido1 <vcgarr94@gmail.com>
2024-03-17 19:28:45 -0700

    adjusting the home page scss

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,16 +1,17 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Layout from './components/Layout';
+import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
-    </Router> // Fix: Replace `</>` with `</Router>`
+    </Router>
   );
 }
 
--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'; // Add missing import statement for React
+import './index.scss'
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm</h1> {/* Fix syntax error */}
-                <img src={LogoTitle} alt="developer" /> {/* Use the imported LogoTitle */}
+                <h1>Hi, <br/> I'm
+                <img src={LogoTitle} alt="developer" /> 
+                Victor <br/> Future Web Developer
+                </h1> 
+                <h2> Full Stack Developer</h2>
+                <Link to="/contact" className='flat-button'>Contact ME</Link>
+
             </div>
         </div> 
     );
