d5a0fca76fa07bd44ff4ffaeb19eff9d958b77d4
victorgarrido1 <vcgarr94@gmail.com>
2024-03-21 17:17:20 -0700

    added contact page

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+
+const Contact = () => {
+    return (
+        <>
+        <div className='container contact-page'></div>
+        <div className='text-zone'></div>
+        <h1>
+            Contact me
+        </h1>
+        </>
+    )
+}
+
+export default Contact
