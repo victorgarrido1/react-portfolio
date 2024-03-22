3b76a0cd65572ebeabfe1fe41fbfaeacb38929a9
victorgarrido1 <vcgarr94@gmail.com>
2024-03-22 01:18:22 -0700

    adding contact page structure

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,15 +1,32 @@
-import './index.scss'
+import './index.scss';
 
 const Contact = () => {
-    return (
-        <>
-        <div className='container contact-page'></div>
-        <div className='text-zone'></div>
-        <h1>
-            Contact me
-        </h1>
-        </>
-    )
+  return (
+    <>
+      <div className="container contact-page "></div>
+      <div className="contact-text text-zone"> {/* Apply appropriate class names */}
+        <h1>Contact me</h1>
+        <p>
+          I am interested in opportunities in web development and software
+          engineering. As a self motivated individual, I take consistent action
+          to learn and dwell to use my skills to good use. If you have any
+          questions, feel free to contact me below{' '}
+        </p>
+        <div className="contact-form">
+            <form>
+                <ul>
+                    <li className="half">
+                        <input type="text" name="name" placeholder="Name" required />
+                        <input type="email" name="email" placeholder="Email" required />
+                        <input placeholder="Subject" type="text" name="subject" required />
+                        <textarea placeholder="Message" name="message" required></textarea>
+                    </li>
+                </ul>
+            </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Contact
+export default Contact;
