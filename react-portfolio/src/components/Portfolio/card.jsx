3cb42b8d28e1e4baf4dfd60db72a6b1928ec07eb
victorgarrido1 <vcgarr94@gmail.com>
2024-03-23 23:20:19 -0700

    putting the Cards into portfolio, will need to convert more better structure. possible map

--- react-portfolio/src/components/Portfolio/card.jsx
+++ react-portfolio/src/components/Portfolio/card.jsx
@@ -1,13 +1,47 @@
-import projectThumbnail from '../../assets/images/project_1'
+import projectThumbnail1 from '../../assets/images/project1.png'
+import projectThumbnail2 from '../../assets/images/portfolio2.png'
+import projectThumbnail3 from '../../assets/images/portfolio3.png'
+import projectThumbnail4 from '../../assets/images/project4.png'
+import projectThumbnail5 from '../../assets/images/project5.png'
+import projectThumbnail6 from '../../assets/images/project6.png'
+
+
 
 // Define a functional component named Card
-const Card = () => {
+const Card = ({ thumbnail, title, description}) => {
     return (
-      <div className="card">
-        <img src={projectThumbnail} alt="Description" />
-        <h2>React</h2>
-        <p>Description of the project</p>
+      <>
+      <div className="projectIcons">
+        <img src={projectThumbnail1} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
+      <div className="projectIcons">
+        <img src={projectThumbnail2} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="projectIcons">
+        <img src={projectThumbnail3} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="projectIcons">
+        <img src={projectThumbnail4} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="projectIcons">
+        <img src={projectThumbnail5} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="projectIcons">
+        <img src={projectThumbnail6} alt="Description" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      </>
     );
   };
   
--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -1,24 +1,30 @@
+// Portfolio.jsx
+
 import React from 'react';
-import './index.scss'; // Import your SCSS file
-import './card.jsx'
+import Card from './card.jsx'; // Importing Card component
 
-
-
-
+import projectThumbnail1 from '../../assets/images/project1.png';
+import projectThumbnail2 from '../../assets/images/portfolio2.png';
+import projectThumbnail3 from '../../assets/images/portfolio3.png';
+import projectThumbnail4 from '../../assets/images/project4.png';
+import projectThumbnail5 from '../../assets/images/project5.png';
+import projectThumbnail6 from '../../assets/images/project6.png';
 
 const Portfolio = () => {
   return (
     <div className="grid-container-template">
       <h1 className='mainText'>Hello</h1>
-      {/* You can add more content here */}
-      <div className='flex-container'></div>
-
-        <div className='flex-container'>1</div>
-
-
+      <div className='flex-container'>
+        {/* Render the Card component with different props */}
+        <Card thumbnail={projectThumbnail1} title="Project 1" description="Description of project 1" />
+        <Card thumbnail={projectThumbnail2} title="Project 2" description="Description of project 2" />
+        <Card thumbnail={projectThumbnail3} title="Project 3" description="Description of project 3" />
+        <Card thumbnail={projectThumbnail4} title="Project 4" description="Description of project 4" />
+        <Card thumbnail={projectThumbnail5} title="Project 5" description="Description of project 5" />
+        <Card thumbnail={projectThumbnail6} title="Project 6" description="Description of project 6" />
+      </div>
     </div>
   );
 };
 
-
-export default Portfolio
+export default Portfolio;
