0b53d4964d606f6e0f99070b320ebe2a24c0a7e7
victorgarrido1 <vcgarr94@gmail.com>
2024-03-26 13:19:27 -0700

    attempting to move images divs to right

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import projectThumbnail1 from '../../assets/images/project1.png';
-import projectThumbnail2 from '../../assets/images/portfolio2.png';
-import projectThumbnail3 from '../../assets/images/portfolio3.png';
-import projectThumbnail4 from '../../assets/images/project4.png';
-import projectThumbnail5 from '../../assets/images/project5.png';
-import projectThumbnail6 from '../../assets/images/project6.png';
+import './index.scss'
+import React from 'react'
+import projectThumbnail1 from '../../assets/images/project1.png'
+import projectThumbnail2 from '../../assets/images/portfolio2.png'
+import projectThumbnail3 from '../../assets/images/portfolio3.png'
+import projectThumbnail4 from '../../assets/images/project4.png'
+import projectThumbnail5 from '../../assets/images/project5.png'
+import projectThumbnail6 from '../../assets/images/project6.png'
 
 const Portfolio = () => {
   // Define the Card component inside the Portfolio component
   const Card = ({ thumbnail, title, description }) => (
     <div className="projectIcons">
       <div className="column">
-        <img src={thumbnail} alt="Description" style={{ maxWidth: '350%', maxHeight: '200px' }} />
+        <img
+          src={thumbnail}
+          alt="Description"
+          style={{ maxWidth: '350%', maxHeight: '200px' }}
+        />
         <h2>{title}</h2>
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 
   // Array of project details
   const projects = [
@@ -50,25 +55,48 @@ const Portfolio = () => {
       title: 'Project 6',
       description: 'Description of project 6',
     },
-  ];
+  ]
 
   return (
-    <div className="grid-container-template" style={{ display: 'flex', flexDirection: 'row', gridGap: '20px' }}>
+    <div
+      className="grid-container-template"
+      style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', gap: '20px' }}>
+    
       {/* Render the first 3 Card components */}
-      <div className="flex-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <div
+        className="flex-container"
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}
+      >
         <h1 className="mainText">Hello</h1>
         {projects.slice(0, 3).map((project, index) => (
-          <Card key={index} thumbnail={project.thumbnail} title={project.title} description={project.description} />
+          <Card
+            key={index}
+            thumbnail={project.thumbnail}
+            title={project.title}
+            description={project.description}
+          />
         ))}
       </div>
       {/* Render the last 3 Card components */}
-      <div className="flex-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <div
+     
+      >
+        {/* Render the last 3 Card components */}
         {projects.slice(3).map((project, index) => (
-          <Card key={index + 3} thumbnail={project.thumbnail} title={project.title} description={project.description} />
+          <Card
+            key={index + 3}
+            thumbnail={project.thumbnail}
+            title={project.title}
+            description={project.description}
+          />
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
